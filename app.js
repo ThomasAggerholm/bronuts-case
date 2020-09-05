3a491f51346c5f3410cfa6c2ddbc17f8ee14ca83
Thomas Aggerholm <thomasaggerholm@hotmail.com>
2020-09-05 17:51:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const 
+    playbackConst = 1000,
+    video = document.querySelector('[data-video]'),
+    body = document.querySelector('body')
+
+let frameNumber = 0
+
+video.addEventListener('loadedmetadata', () => {
+    body.style.height = `${Math.floor(video.duration) * playbackConst}px`
+})
+
+const scrollPlay = () => {  
+    var frameNumber  = window.pageYOffset/playbackConst
+    video.currentTime  = frameNumber
+    window.requestAnimationFrame(scrollPlay)
+}
+
+window.requestAnimationFrame(scrollPlay)
